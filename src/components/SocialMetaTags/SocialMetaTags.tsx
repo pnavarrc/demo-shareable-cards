899f7eb4f11e395d825581330ceb915a162895e8
Pablo Navarro Castillo <pnavarrc@gmail.com>
2021-07-23 18:35:33 -0700

    fix meta tags

--- src/components/SocialMetaTags/SocialMetaTags.tsx
+++ src/components/SocialMetaTags/SocialMetaTags.tsx
@@ -1,5 +1,3 @@
-import Head from "next/head";
-
 const SocialMetaTags: React.FC<{
   site: string;
   creator: string;
@@ -8,13 +6,14 @@ const SocialMetaTags: React.FC<{
   imageUrl: string;
 }> = ({ site, creator, title, description, imageUrl }) => {
   return (
-    <Head>
-      <meta name="twitter:site" content={site} />
-      <meta name="twitter:creator" content={creator} />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={imageUrl} />
-    </Head>
+    <>
+      <meta name="twitter:card" content="summary_large_image"></meta>
+      <meta name="twitter:site" content={site}></meta>
+      <meta name="twitter:creator" content={creator}></meta>
+      <meta name="twitter:title" content={title}></meta>
+      <meta name="twitter:description" content={description}></meta>
+      <meta name="twitter:image" content={imageUrl}></meta>
+    </>
   );
 };
 
