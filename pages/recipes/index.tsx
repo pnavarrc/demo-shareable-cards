e744d30b1048d8f92925ef3f9fdfe730e78378a2
Pablo Navarro Castillo <pnavarrc@gmail.com>
2021-08-03 13:10:55 -0700

    direct under heat (meta)

--- pages/recipes/index.tsx
+++ pages/recipes/index.tsx
@@ -27,40 +27,40 @@ const RecipesHomePage: React.FC = () => {
         <meta
           name="twitter:title"
           key="twitter_title"
-          content={"Apple recipes"}
+          content="Apple recipes"
         />
         <meta
           name="twitter:description"
           key="twitter_description"
-          content={"generic description"}
+          content="generic description"
         />
         <meta
           name="twitter:image"
           key="twitter_image"
           content="https://cosmiccrisp.com/wp-content/uploads/2021/03/Apple-Date-Crisp-e1614727396388-600x500.png"
         />
-        {/* Shared content */}
-        {sharedRecipe && (
-          <>
-            <meta
-              name="twitter:title"
-              key="twitter_title"
-              content={sharedRecipe.title}
-            />
-            <meta
-              name="twitter:description"
-              key="twitter_description"
-              content={sharedRecipe.description}
-            />
-            <meta
-              name="twitter:image"
-              key="twitter_image"
-              content={sharedRecipe.imgUrl}
-            />
-            <title key="title">{sharedRecipe.title}</title>
-          </>
-        )}
       </Head>
+      {/* Shared content */}
+      {sharedRecipe && (
+        <Head>
+          <meta
+            name="twitter:title"
+            key="twitter_title"
+            content={sharedRecipe.title}
+          />
+          <meta
+            name="twitter:description"
+            key="twitter_description"
+            content={sharedRecipe.description}
+          />
+          <meta
+            name="twitter:image"
+            key="twitter_image"
+            content={sharedRecipe.imgUrl}
+          />
+          <title key="title">{sharedRecipe.title}</title>
+        </Head>
+      )}
       <Container>
         <Typography variant="h1">Apple Recipes</Typography>
         <Grid container spacing={2}>
